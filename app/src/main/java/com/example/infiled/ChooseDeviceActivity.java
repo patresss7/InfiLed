e1d045ab7066b38d83045b4f3bb4386678a53f6a
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-10-29 21:09:47 +0100

    Choose effect added
    Empty choose effect activity that appears after clicking on text Choose device

--- app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
+++ app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
@@ -2,7 +2,9 @@ package com.example.infiled;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChooseDeviceActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class ChooseDeviceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_device);
+    }
+
+    public void onChooseDeviceClick(View view) {
+        Intent intent = new Intent(this, ChooseEffectActivity.class);
+        startActivity(intent);
     }
 }
