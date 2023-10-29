d3fa774689de4227ee9231ca966036dc228f5dea
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-10-29 20:59:11 +0100

    Choose device activity added
    Activity appears 2 seconds after launch of the app

--- app/src/main/java/com/example/infiled/BootMessageActivity.java
+++ app/src/main/java/com/example/infiled/BootMessageActivity.java
@@ -2,7 +2,10 @@ package com.example.infiled;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class BootMessageActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class BootMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boot_message);
+
+        // Create a Handler to delay launching the DelayedActivity
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Create an Intent to start the DelayedActivity
+                Intent intent = new Intent(BootMessageActivity.this, ChooseDeviceActivity.class);
+                startActivity(intent);
+            }
+        }, 2000); // 2-second delay
     }
 }
