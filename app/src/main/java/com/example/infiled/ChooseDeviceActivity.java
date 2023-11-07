ac6054ce590cb911462d960500d6d04b105f71be
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-11-07 22:10:57 +0100

    Initial check permissions added

--- app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
+++ app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
@@ -1,10 +1,14 @@
 package com.example.infiled;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.Manifest;
 
 public class ChooseDeviceActivity extends AppCompatActivity {
 
@@ -12,7 +16,35 @@ public class ChooseDeviceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_device);
+
+        checkAllPermissions();
     }
+    
+    private void checkAllPermissions(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_ADMIN) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+
+            System.out.println("Permissions granting requested!");
+
+            // Request permissions
+            ActivityCompat.requestPermissions(this, new String[] {
+                    Manifest.permission.BLUETOOTH,
+                    Manifest.permission.BLUETOOTH_SCAN,
+                    Manifest.permission.BLUETOOTH_CONNECT,
+                    Manifest.permission.BLUETOOTH_ADMIN,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            }, 1);
+        }
+        else{
+            System.out.println("Permissions already granted!");
+        }
+    }
+    
 
     public void onChooseDeviceClick(View view) {
         Intent intent = new Intent(this, ChooseEffectActivity.class);
