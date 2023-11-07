f918d11c971953a4d3056de871efa09e84e59ee3
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-11-07 23:06:14 +0100

    Bluetooth get available devices once

--- app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
+++ app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
@@ -1,5 +1,7 @@
 package com.example.infiled;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +10,34 @@ import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.Manifest;
 
 public class ChooseDeviceActivity extends AppCompatActivity {
 
     private BluetoothAdapter bluetoothAdapter;
+
+    private final BroadcastReceiver discoveryReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                if(checkCustomPermission(Manifest.permission.BLUETOOTH_CONNECT))
+                    Log.d(TAG, "Found device: " + device.getName() + " - " + device.getAddress());
+                // You can add the discovered device to a list or adapter for display.
+            }
+        }
+
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +48,30 @@ public class ChooseDeviceActivity extends AppCompatActivity {
         bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         enableBluetooth(bluetoothAdapter);
+
+        discoverDevices(bluetoothAdapter);
+    }
+
+    private void discoverDevices(BluetoothAdapter bluetoothAdapter) {
+        if(checkCustomPermission(Manifest.permission.BLUETOOTH_SCAN)) {
+            if (bluetoothAdapter.isDiscovering()) {
+                bluetoothAdapter.cancelDiscovery();
+            }
+        }
+
+        // Register for broadcasts when a device is discovered.
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(discoveryReceiver, filter);
+
+        // Start discovery.
+        bluetoothAdapter.startDiscovery();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Unregister the broadcast receiver when the activity is destroyed.
+        unregisterReceiver(discoveryReceiver);
     }
 
     private void enableBluetooth(BluetoothAdapter bluetoothAdapter){
@@ -51,6 +96,11 @@ public class ChooseDeviceActivity extends AppCompatActivity {
             }
         }
     }
+
+    private boolean checkCustomPermission(String permission){
+        return ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+    }
+
 
     private void checkAllPermissions(){
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
