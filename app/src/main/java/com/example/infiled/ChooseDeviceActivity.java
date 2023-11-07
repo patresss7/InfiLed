fbf5fe85026c4db15e36219527f9de49d7787da5
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-11-07 22:34:48 +0100

    Bluetooth enabled check

--- app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
+++ app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
@@ -1,9 +1,13 @@
 package com.example.infiled;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -12,14 +16,42 @@ import android.Manifest;
 
 public class ChooseDeviceActivity extends AppCompatActivity {
 
+    private BluetoothAdapter bluetoothAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_device);
 
         checkAllPermissions();
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        enableBluetooth(bluetoothAdapter);
     }
-    
+
+    private void enableBluetooth(BluetoothAdapter bluetoothAdapter){
+        if(bluetoothAdapter == null){
+            System.out.println("Problem with setting up Bluetooth!");
+        }
+        else{
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+                        result -> {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+                                System.out.println("Bluetooth was enabled successfully");
+                            } else {
+                                System.out.println("Bluetooth enabling failed.");
+                            }
+                        });
+                launcher.launch(enableBtIntent);
+            }
+            else{
+                System.out.println("Bluetooth already enabled");
+            }
+        }
+    }
+
     private void checkAllPermissions(){
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED ||
                 ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED ||
