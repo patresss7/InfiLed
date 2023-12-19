cd72f6873421957c379611cc4dea55edbc1bed55
patresss7 <75572905+patresss7@users.noreply.github.com>
2023-12-19 16:53:47 +0100

    probably succesfully connected device

--- app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
+++ app/src/main/java/com/example/infiled/ChooseDeviceActivity.java
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -25,8 +26,11 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.security.Permission;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class ChooseDeviceActivity extends AppCompatActivity {
 
@@ -37,6 +41,9 @@ public class ChooseDeviceActivity extends AppCompatActivity {
     private TextView textView1;
     private TextView textView2;
     private TextView textView3;
+    private BluetoothDevice targetDevice;
+    private BluetoothSocket bluetoothSocket;
+    private UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
 
     private final BroadcastReceiver discoveryReceiver = new BroadcastReceiver() {
@@ -182,7 +189,83 @@ public class ChooseDeviceActivity extends AppCompatActivity {
             System.out.println("Permissions already granted!");
         }
     }
-    
+
+    public void onDevice1Click(View view){
+        BluetoothDevice device = availableInfiLedDevicesList.get(0);
+        new ConnectThread(device).start();
+    }
+
+    public void onDevice2Click(View view){
+
+    }
+
+    public void onDevice3Click(View view){
+
+    }
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            // Use a temporary object that is later assigned to mmSocket
+            // because mmSocket is final.
+            BluetoothSocket tmp = null;
+            mmDevice = device;
+
+            try {
+                // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                // MY_UUID is the app's UUID string, also used in the server code.
+                if(checkCustomPermission(Manifest.permission.BLUETOOTH_CONNECT))
+                    tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "Socket's create() method failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            // Cancel discovery because it otherwise slows down the connection.
+            if(checkCustomPermission(Manifest.permission.BLUETOOTH_SCAN))
+                bluetoothAdapter.cancelDiscovery();
+
+            try {
+                // Connect to the remote device through the socket. This call blocks
+                // until it succeeds or throws an exception.
+                mmSocket.connect();
+
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and return.
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) {
+                    Log.e(TAG, "Could not close the client socket", closeException);
+                }
+            }
+
+            // The connection attempt succeeded. Perform work associated with
+            // the connection in a separate thread.
+            Log.d(TAG, "Connection attempt succeeded!");
+            runOnUiThread(() -> {
+                // Move to the ChooseEffectActivity after successful connection.
+                Intent intent = new Intent(ChooseDeviceActivity.this, ChooseEffectActivity.class);
+                startActivity(intent);
+
+                // Close the current activity (optional, depending on your requirements).
+                finish();
+            });
+            //manageMyConnectedSocket(mmSocket);
+        }
+
+        // Closes the client socket and causes the thread to finish.
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Could not close the client socket", e);
+            }
+        }
+    }
 
     public void onChooseDeviceClick(View view) {
         Intent intent = new Intent(this, ChooseEffectActivity.class);
